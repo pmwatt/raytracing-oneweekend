80a75d85d0b936efc2930db6de9d6e133ba98678
pmwatt <prachnachai.mek@student.mahidol.edu>
2023-09-11 11:35:42 +0700

    add ray.h for P(t) = A + bt

---
+++ raytracing-oneweekend/ray.h
@@ -0,0 +1,27 @@
+#ifndef RAY_H
+#define RAY_H
+
+#include "vec3.h"
+
+class ray {
+public:
+	ray() {}
+
+	ray (const point3& origin, const vec3& direction) :
+		orig(origin), dir(direction) {}
+
+	point3 origin() const { return orig; }
+	vec3 direction() const { return dir; }
+
+	// P(t) = A + tB
+	point3 at(double t) const {
+		return orig + t * dir;
+	}
+ 
+private:
+	// P(t) = A + tb
+	point3 orig; // A
+	vec3 dir; // b
+};
+
+#endif
